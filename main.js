36d62ad170d72d5770cae6773ed3db6f9b8a7a97
Amanda Slayton <alslayton86@gmail.com>
2018-03-22 19:53:30 -0500

    writeToDom Function added

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+//ARE WE LINKED?
 console.log("Why, hello there");
+
+//WRITE LOCATION CARDS TO DOM FUNCTION
+const writeToDom = (myInnerds, writeHere) => {
+  const myElement = document.getElementById(writeHere);
+  myElement.innerHTML = myInnerds;
+};
+
+
